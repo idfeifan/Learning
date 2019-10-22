203d63c71b60e77e343fdd96e5f4a4ed57c7bedd
spark学习 <358073662@qq.com>
2019-10-22 15:54:43 +0800

    20191022

--- Python从入门到实践/项目/外星人入侵/alien_invasion.py
+++ Python从入门到实践/项目/外星人入侵/alien_invasion.py
@@ -1,8 +1,12 @@
 # coding=utf-8
 import pygame
+#类的实例类似列表提供了有助于开发游戏的额外功能
+from pygame.sprite import Group
 from Python从入门到实践.项目.外星人入侵.settings import Settings
 from Python从入门到实践.项目.外星人入侵.ship import Ship
+from Python从入门到实践.项目.外星人入侵.bullet import Bullet
 import Python从入门到实践.项目.外星人入侵.game_functions as gf
+from Python从入门到实践.项目.外星人入侵.Alien import Alien
 def run_game():
     '''初始化游戏并创建屏幕对象'''
     pygame.init()
@@ -15,12 +19,21 @@ def run_game():
 
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+
+    #创建一个外星人实例
+    alien = Alien(ai_settings,screen)
 
     #开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
-        gf.check_events(ship)
-        ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update() #更新飞船的位置
+        gf.update_bullets(bullets) #所有未消失的子弹的位置
+        gf.update_screen(ai_settings,screen,ship,alien,bullets) #更新后的位置来绘制屏幕
 
-run_game()
+
+
+if __name__ == "__main__":
+    run_game()
--- Python从入门到实践/项目/外星人入侵/settings.py
+++ Python从入门到实践/项目/外星人入侵/settings.py
@@ -9,3 +9,11 @@ class Settings():
         self.screen_height = 1000
         self.bg_color = (230,230,230)
         self.ship_speed_factor = 1.5
+
+        #子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60,60,60
+
+        self.bullet_allowed = 3
