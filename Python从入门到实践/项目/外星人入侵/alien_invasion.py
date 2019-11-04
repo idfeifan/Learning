ea0d269b723704c78b4131d2709d07733a76d351
spark学习 <358073662@qq.com>
2019-11-04 09:47:08 +0800

    20191022

--- Python从入门到实践/项目/外星人入侵/alien_invasion.py
+++ Python从入门到实践/项目/外星人入侵/alien_invasion.py
@@ -6,6 +6,10 @@ from Python从入门到实践.项目.外星人入侵.settings import Settings
 from Python从入门到实践.项目.外星人入侵.ship import Ship
 from Python从入门到实践.项目.外星人入侵.bullet import Bullet
 import Python从入门到实践.项目.外星人入侵.game_functions as gf
+from Python从入门到实践.项目.外星人入侵.game_stats import GameStats
+from Python从入门到实践.项目.外星人入侵.button import Button
+from Python从入门到实践.项目.外星人入侵.scoreboard import Scoreboard
+
 def run_game():
     '''初始化游戏并创建屏幕对象'''
     pygame.init()
@@ -15,6 +19,12 @@ def run_game():
         (ai_settings.screen_height,ai_settings.screen_width))
     pygame.display.set_caption("Alien Invasion")
 
+    #创建Play按钮
+    play_button = Button(ai_settings,screen,"Play")
+
+    #创建一个用于存储游戏统计信息的实例,并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings,screen,stats)
 
     #创建一艘飞船、一个子弹编组 和 一个外星人编组
     ship = Ship(ai_settings,screen)
@@ -27,11 +37,14 @@ def run_game():
     #开始游戏的主循环
     while True:
         #监视键盘和鼠标事件
-        gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update() #更新飞船的位置
-        gf.update_bullets(bullets) #所有未消失的子弹的位置
-        gf.update_aliens(aliens)
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets) #更新后的位置来绘制屏幕
+        gf.check_events(ai_settings,screen,stats,sb,play_button,ship,aliens,bullets)
+        if stats.game_active:
+            ship.update() #更新飞船的位置
+            gf.update_bullets(ai_settings,screen,stats,sb,ship,aliens,bullets) #所有未消失的子弹的位置
+            gf.update_aliens(ai_settings,stats,screen,sb,ship,aliens,bullets)
+
+        gf.update_screen(ai_settings,screen,stats,sb,
+                         ship,aliens,bullets,play_button) #更新后的位置来绘制屏幕
 
 
 
--- Python从入门到实践/项目/外星人入侵/ship.py
+++ Python从入门到实践/项目/外星人入侵/ship.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 import pygame
+from pygame.sprite import Sprite
 '''管理飞船大部分行为'''
-class Ship():
+class Ship(Sprite):
 
     def __init__(self,ai_settings,screen):
         """初始化飞船并设置其初始位置"""
+        super(Ship,self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
         #加载飞船图像并获取其外接矩形
@@ -33,6 +35,9 @@ class Ship():
         #根据self.center跟新到rect对象
 
         self.rect.centerx = self.center
+    def center_ship(self):
+        """让飞船在屏幕上居中"""
+        self.center = self.screen_rect.centerx
 
     def blitme(self):
         """在指定位置绘制飞船"""
