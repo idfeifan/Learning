4e84697c67b05b527fccc358409b8341eb30fe93
非凡 <358073662@qq.com>
2020-07-18 14:27:37 +0800

    脚本

--- 脚本/修改字符集和编码.py
+++ 脚本/修改字符集和编码.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Time : 2019/12/2 11:22
+
+import os
+import chardet
+'''
+@Time : 2019/12/2 11:22
 # @Author : way
 # @Site :
 # @Describe: 检查 脚本文件 转换格式为 LF , utf-8
-
-import sys
-import os
-import chardet
-
+'''
 def turn(file):
     #打开文件
     with open(file, 'rb') as f:
@@ -18,13 +16,22 @@ def turn(file):
         tp = 'LF'
         if '\r' in data_str:
             tp = 'Macintosh(CR)'
-            data_str = data_str.replace('\r', '\n')
+            data_str = data_str.replace('\r', '\n') #替换为unix 格式
         if encoding not in ['utf-8', 'ascii'] or tp == 'Macintosh(CR)':
             with open(file, 'w', newline='\n', encoding='utf-8') as f:
-                f.write(data_str)
+                f.write(data_str)  #替换为unix 格式，并且转换格式为utf-8
             print(f"{file}: ({tp},{encoding}) trun to (LF,utf-8) success!")
 if __name__ == "__main__":
-    dr = r'D:\SVN\产品线\数据治理\南通市社会治理现代化指挥中心数据资源库建设项目\项目实施资料\hive开发\20200702MySql迁移\市政园林局\wu'
-    for path in os.listdir(dr):
-        file = os.path.join(dr, path)
-        turn(file)
+    dr = r'D:\SVN\产品线\数据治理\南通市社会治理现代化指挥中心数据资源库建设项目\项目实施资料\hive开发\临时存储\20200701'
+    #root 表示上级目录， dirs，files内在文件和目录的列表，如果目录下文件为空，则files为空列表
+    for root,dirs,files in os.walk(dr):
+        #遍历文件
+        for file in files:
+            #切割文件名称和类型
+            f=os.path.splitext(file)
+            filename,type = f
+            #判断文件是为.sh结尾文件
+            if type == '.sh':
+                #拼接文件
+                file_name = os.path.join(root,file)
+                turn(file_name)
